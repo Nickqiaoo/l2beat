aa60e7e607a8234b7c2915621253ad46010496d6
Nickqiaoo <nickqiaoo@gmail.com>
2024-07-08 23:19:57 +0800

    feat: optimization btc balance query

--- packages/backend/src/modules/tvl/balances/BitcoinBalanceProvider.ts
+++ packages/backend/src/modules/tvl/balances/BitcoinBalanceProvider.ts
@@ -7,10 +7,20 @@ export interface BitcoinBalanceQuery {
   address: string
 }
 
+type CacheEntry = {
+  timestamp: number,
+  transactions: any[]
+};
+
 export class BitcoinBalanceProvider implements BalanceProvider {
+  private cache: Map<string, CacheEntry>;
+  private cacheTimeout: number;
   constructor(
     private readonly blockchainClient: BlockchainClient,
-  ) {}
+  ) {
+    this.cache = new Map<string, CacheEntry>();
+    this.cacheTimeout = 3600 * 1000; // 缓存超时设置为1小时
+  }
 
   public getChainId(): ChainId {
     return ChainId(0)
@@ -29,15 +39,20 @@ export class BitcoinBalanceProvider implements BalanceProvider {
       let finalBalance = 0;
   
       while (hasMorePages) {
-        const transactions = await this.blockchainClient.getAddressTransactions(query.holder.toString(), 50, page*50);
-        const balanceAtBlock = this.calculateBalanceAtBlock(transactions.txs, blockHeight);
+        const transactions = await this.getCachedAddressTransactions(query.holder.toString(), page*50);
+        const balanceAtBlock = this.calculateBalanceAtBlock(transactions, blockHeight);
   
         if (balanceAtBlock !== null) {
           finalBalance = balanceAtBlock;
           break; // 找到指定区块高度的余额后退出循环
         }
   
-        hasMorePages = transactions.txs.length > 0; // 假设API在没有更多交易时返回空数组
+         // 如果当前页的交易记录中最大区块高度已经小于目标区块高度，则不再需要继续请求
+         if (transactions.length > 0 && transactions[transactions.length - 1].block_height < blockHeight) {
+          break;
+        }
+        
+        hasMorePages = transactions.length > 0; // 假设API在没有更多交易时返回空数组
         page++;
       }
   
@@ -52,6 +67,32 @@ export class BitcoinBalanceProvider implements BalanceProvider {
   
 
     return balances
+  }
+
+  private async getCachedAddressTransactions(holder: string, offset: number): Promise<any[]> {
+    const cacheKey = `${holder}-${offset}`;
+    const currentTime = Date.now();
+
+    // 检查缓存是否存在且未超时
+    if (this.cache.has(cacheKey)) {
+      const cacheEntry = this.cache.get(cacheKey)!;
+      if (currentTime - cacheEntry.timestamp < this.cacheTimeout) {
+        return cacheEntry.transactions;
+      } else {
+        this.cache.delete(cacheKey); // 删除超时的缓存
+      }
+    }
+
+    // 调用实际的API获取数据
+    const transactions = await this.blockchainClient.getAddressTransactions(holder, 50, offset);
+
+    // 将结果缓存
+    this.cache.set(cacheKey, {
+      timestamp: currentTime,
+      transactions: transactions.txs
+    });
+
+    return transactions.txs;
   }
 
   private calculateBalanceAtBlock(txs: any[], blockHeight: number): number | null {
